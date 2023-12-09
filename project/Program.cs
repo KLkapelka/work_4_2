93e7b4f3b6b8120e6fce26aac593ab61c5ea9aa1
KLkapelka <153184633+KLkapelka@users.noreply.github.com>
2023-12-09 10:39:55 +0700

    Create Program.cs

---
+++ project/Program.cs
@@ -0,0 +1,31 @@
+﻿// Дан файл numsTask2.txt с вещественными числами, расположенными через «;» Переберите все числа до 0.
+// Определите сумму положительных элементов получившейся последовательности;
+
+using System;
+using System.IO;
+class work_4_2
+{
+    static void Main()
+    {
+        string fael = "numsTask2.txt"; 
+        string[] faelNumbers = File.ReadAllText(fael).Split(';'); 
+        float sum = 0;
+
+        foreach (string i in faelNumbers)
+        {
+            if (float.TryParse(i, out float number)) 
+                if (number > 0)
+                {
+                    sum += number;
+                }
+                else
+                {
+                    Console.WriteLine($"Сумма положительный чисел: {sum}");
+                    break;
+                }
+            }
+        }
+    }
+
+}
+
